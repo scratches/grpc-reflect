48b5d8205765c11d99441924d821e4204d549bc8
Dave Syer <david.syer@broadcom.com>
2025-06-11 17:07:50 +0100

    Add prototype for bindable service

---
+++ bindable/src/main/java/org/springframework/grpc/sample/GrpcRestController.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2024-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.grpc.sample;
+
+import org.springframework.grpc.sample.proto.HelloReply;
+import org.springframework.grpc.sample.proto.HelloRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.protobuf.Message;
+
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.ServerCall;
+import io.grpc.ServerMethodDefinition;
+import io.grpc.Status;
+
+@RestController
+public class GrpcRestController {
+
+	@PostMapping(path = "Simple/SayHello", produces = "application/grpc")
+	public HelloReply service(@RequestBody HelloRequest input) {
+		GrpcServerService service = new GrpcServerService();
+		ServerMethodDefinition<Object, Object> serverMethod = (ServerMethodDefinition<Object, Object>) service.bindService().getMethod("Simple/SayHello");
+		var method = serverMethod.getMethodDescriptor();
+		var call = new LocalServerCall<Object, Object>(method);
+		var listener = serverMethod.getServerCallHandler().startCall(call, null);
+		listener.onMessage(input);
+		listener.onReady();
+		listener.onHalfClose();
+		listener.onComplete();
+		HelloReply entity = (HelloReply) call.response;
+		return entity;
+	}
+
+	class LocalServerCall<Req,Res> extends ServerCall<Req,Res> {
+		private MethodDescriptor<Req,Res> method;
+		private Object response;
+
+		public LocalServerCall(MethodDescriptor<Req, Res> method) {
+			this.method = method;
+		}
+
+		@Override
+		public void request(int numMessages) {
+			System.out.println("Requesting " + numMessages + " messages");
+		}
+
+		@Override
+		public void sendMessage(Res message) {
+			System.out.println("Sending message: " + message);
+			this.response = message;
+		}
+
+		@Override
+		public void close(Status status, Metadata trailers) {
+			System.out.println("Call closed with status: " + status);
+		}
+
+		@Override
+		public boolean isCancelled() {
+			return false;
+		}
+
+		@Override
+		public void sendHeaders(Metadata headers) {
+			System.out.println("Sending headers: " + headers);
+		}
+
+		@Override
+		public MethodDescriptor<Req,Res> getMethodDescriptor() {
+			return method;
+		}
+
+	}
+
+}
