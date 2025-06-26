adc4fe17db18e9aa3b4232292a987429ad3ce09c
Dave Syer <david.syer@broadcom.com>
2025-06-26 14:40:56 +0100

    Add reactive test

---
+++ bindable/src/test/java/org/springframework/grpc/sample/GrpcReactorServerApplicationTests.java
@@ -0,0 +1,53 @@
+package org.springframework.grpc.sample;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.grpc.client.ImportGrpcClients;
+import org.springframework.grpc.sample.proto.HelloReply;
+import org.springframework.grpc.sample.proto.HelloRequest;
+import org.springframework.grpc.sample.proto.ReactorHelloGrpc.ReactorHelloStub;
+import org.springframework.test.annotation.DirtiesContext;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
+		"spring.grpc.client.default-channel.address=0.0.0.0:${local.server.port}" })
+@DirtiesContext
+public class GrpcReactorServerApplicationTests {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GrpcServerApplication.class).run();
+	}
+
+	@Autowired
+	private ReactorHelloStub stub;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void serverResponds() {
+		HelloReply response = stub.sayHello(Mono.just(HelloRequest.newBuilder().setName("Alien").build())).block();
+		assertEquals("Hello ==> Alien", response.getMessage());
+	}
+
+	@Test
+	void serverStreams() {
+		HelloReply response = stub.parallelHello(Flux.just(HelloRequest.newBuilder().setName("Alien").build())).blockFirst();
+		assertEquals("Hello ==> Alien", response.getMessage());
+	}
+
+	@TestConfiguration
+	@ImportGrpcClients(types =  ReactorHelloStub.class)
+	static class ExtraConfiguration {
+	}
+
+}
